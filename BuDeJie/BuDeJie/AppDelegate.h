0668f55f49d1c288fc251103f83b46ce351c392c
xiaoyouPrince <xiaoyouPrince@163.com>
2017-09-08 16:51:14 +0800

    项目基本部署

---
+++ BuDeJie/BuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJie
+//
+//  Created by 渠晓友 on 2017/9/8.
+//  Copyright © 2017年 XiaoYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
